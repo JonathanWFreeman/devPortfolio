8c2b009f2f2cf146f1cfbaf75d879f0b98a17dcf
Jonathan Freeman <JonathanWFreeman@gmail.com>
2020-02-28 12:57:03 -0600

    Update meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Jonathan Freeman - Front End Developer',
+    description: 'Portfolio for Jonathan Freeman - Front End Developer',
+    author: 'Jonathan Freeman',
   },
   plugins: [
     'gatsby-plugin-eslint',
